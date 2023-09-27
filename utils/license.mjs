1efc95a9faf50cbca1691facb7ae7fb2582e3bea
Snap <XxSnapperGeexX@gmail.com>
2023-09-26 23:52:01 -0700

    Create root license object.

--- utils/license.mjs
+++ utils/license.mjs
@@ -1,63 +1,143 @@
 const licenseBadgeImgUrlRoot = Object.freeze({
-    shields: "https://img.shields.io/badge/",
-    buttons: "https://licensebuttons.net/l/"
+    shield: "https://img.shields.io/badge/",
+    button: "https://licensebuttons.net/l/"
 });
 
-const licenseBadgeImgUrlQuery = Object.freeze({
-    apache: "License-Apache_2.0-blue.svg",
-    boost: "License-Boost_1.0-lightblue.svg",
-    bsd2: "License-BSD_2--Clause-orange.svg",
-    bsd3: "License-BSD_3--Clause-blue.svg",
-    eclipse: "License-EPL_1.0-red.svg",
-    gnuAgpl: "License-AGPL_v3-blue.svg",
-    gnuFdl: "License-FDL_v1.3-blue.svg",
-    gnuGpl2: "License-GPL_v2-blue.svg",
-    gnuGpl3: "License-GPLv3-blue.svg",
-    gnuLgpl: "License-LGPL_v3-blue.svg",
-    hippocratic2: "License-Hippocratic_2.1-lightgrey.svg",
-    hippocratic3: "License-Hippocratic_3.0-lightgrey.svg",
-    ibm: "License-IPL_1.0-blue.svg",
-    isc: "License-ISC-blue.svg",
-    mit: "License-MIT-yellow.svg",
-    mozilla: "License-MPL_2.0-brightgreen.svg",
-    attribution: "License-ODC_BY-brightgreen.svg",
-    openDataBase: "License-ODbL-brightgreen.svg",
-    publicDomain: "License-PDDL-brightgreen.svg",
-    perl: "License-Perl-0298c3.svg",
-    artistic: "License-Artistic_2.0-0298c3.svg",
-    sil: "License-OFL_1.1-lightgreen.svg",
-    unlicense: "license-Unlicense-blue.svg",
-    wtfpl: "License-WTFPL-brightgreen.svg",
-    zlib: "License-Zlib-lightgrey.svg"
-});
+export const license = Object.freeze({
+    apache: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Apache_2.0-blue.svg"
+    }),
 
-const creativeCommonLicenseBadgeImgUrlQuery = Object.freeze({
-    cco: Object.freeze({
-        shield: "License-CC0_1.0-lightgrey.svg",
-        button: "zero/1.0/80x15.png"
+    attribution: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-ODC_BY-brightgreen.svg"
     }),
-    attribution4: Object.freeze({
-        shield: "License-CC_BY_4.0-lightgrey.svg",
-        button: "by/4.0/80x15.png"
-    }),
-    attribution4ShareAlike: Object.freeze({
-        shield: "License-CC_BY--SA_4.0-lightgrey.svg",
-        button: "by-sa/4.0/80x15.png"
-    }),
-    attributionNonCommercial: Object.freeze({
-        shield: "License-CC_BY--NC_4.0-lightgrey.svg",
-        button: "by-nc/4.0/80x15.png"
-    }),
+
     attributionNoDerivatives: Object.freeze({
-        shield: "License-CC_BY--ND_4.0-lightgrey.svg",
-        button: "by-nd/4.0/80x15.png"
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY--ND_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by-nd/4.0/80x15.png"
     }),
-    attributionNonCommercialShareAlike: Object.freeze({
-        shield: "License-CC_BY--NC--SA_4.0-lightgrey.svg",
-        button: "by-nc-sa/4.0/80x15.png"
+
+    attributionNonCommercial: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY--NC_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by-nc/4.0/80x15.png"
     }),
+
     attributionNonCommercialNoDerivatives: Object.freeze({
-        shield: "License-CC_BY--NC--ND_4.0-lightgrey.svg",
-        button: "by-nc-nd/4.0/80x15.png"
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY--NC--ND_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by-nc-nd/4.0/80x15.png"
+    }),
+
+    attributionNonCommercialShareAlike: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY--NC--SA_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by-nc-sa/4.0/80x15.png"
+    }),
+
+    attribution4: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by/4.0/80x15.png"
+    }),
+
+    attribution4ShareAlike: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC_BY--SA_4.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "by-sa/4.0/80x15.png"
+    }),
+
+    boost: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Boost_1.0-lightblue.svg"
+    }),
+
+    bsd2: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-BSD_2--Clause-orange.svg"
+    }),
+
+    bsd3: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-BSD_3--Clause-blue.svg"
+    }),
+
+    cco: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-CC0_1.0-lightgrey.svg",
+        buttonLink: licenseBadgeImgUrlRoot.button + "zero/1.0/80x15.png"
+    }),
+
+    eclipse: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-EPL_1.0-red.svg"
+    }),
+
+    gnuAgpl: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-AGPL_v3-blue.svg"
+    }),
+
+    gnuFdl: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-FDL_v1.3-blue.svg"
+    }),
+
+    gnuGpl2: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-GPL_v2-blue.svg"
+    }),
+
+    gnuGpl3: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-GPLv3-blue.svg"
+    }),
+
+    gnuLgpl: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-LGPL_v3-blue.svg"
+    }),
+
+    hippocratic2: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Hippocratic_2.1-lightgrey.svg"
+    }),
+
+    hippocratic3: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Hippocratic_3.0-lightgrey.svg"
+    }),
+
+    ibm: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-IPL_1.0-blue.svg"
+    }),
+
+    isc: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-ISC-blue.svg"
+    }),
+
+    mit: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-MIT-yellow.svg"
+    }),
+
+    mozilla: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-MPL_2.0-brightgreen.svg"
+    }),
+
+    openDataBase: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-ODbL-brightgreen.svg"
+    }),
+
+    publicDomain: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-PDDL-brightgreen.svg"
+    }),
+
+    perl: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Perl-0298c3.svg"
+    }),
+
+    artistic: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Artistic_2.0-0298c3.svg"
+    }),
+
+    sil: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-OFL_1.1-lightgreen.svg"
+    }),
+
+    unlicense: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "license-Unlicense-blue.svg"
+    }),
+
+    wtfpl: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-WTFPL-brightgreen.svg"
+    }),
+
+    zlib: Object.freeze({
+        shieldLink: licenseBadgeImgUrlRoot.shield + "License-Zlib-lightgrey.svg"
     })
 });
+
+export default license;
