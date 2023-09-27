03bfbd9657c177ad7f4831bc73d5e39627efb601
Snap <XxSnapperGeexX@gmail.com>
2023-09-27 00:19:40 -0700

    Implement renderLicenseBadge function.

--- utils/generateMarkdown.mjs
+++ utils/generateMarkdown.mjs
@@ -1,9 +1,8 @@
+import { license as definedLicense } from "./license.mjs";
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-export const renderLicenseBadge = (license) =>
-{
-
-}
+export const renderLicenseBadge = (license) => Object.keys(definedLicense).includes(license) ? `![${definedLicense[license].name}](${definedLicense[license].shieldLink})`: "";
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
