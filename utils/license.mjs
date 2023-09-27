085b92714cb978246536b292d2f131d60f746464
Snap <XxSnapperGeexX@gmail.com>
2023-09-26 22:56:55 -0700

    Init license.mjs with url root and query objects.

---
+++ utils/license.mjs
@@ -0,0 +1,63 @@
+const licenseUrlRoot = Object.freeze({
+    shields: "https://img.shields.io/badge/",
+    buttons: "https://licensebuttons.net/l/"
+});
+
+const licenseUrlQuery = Object.freeze({
+    apache: "License-Apache_2.0-blue.svg",
+    boost: "License-Boost_1.0-lightblue.svg",
+    bsd2: "License-BSD_2--Clause-orange.svg",
+    bsd3: "License-BSD_3--Clause-blue.svg",
+    eclipse: "License-EPL_1.0-red.svg",
+    gnuAgpl: "License-AGPL_v3-blue.svg",
+    gnuFdl: "License-FDL_v1.3-blue.svg",
+    gnuGpl2: "License-GPL_v2-blue.svg",
+    gnuGpl3: "License-GPLv3-blue.svg",
+    gnuLgpl: "License-LGPL_v3-blue.svg",
+    hippocratic2: "License-Hippocratic_2.1-lightgrey.svg",
+    hippocratic3: "License-Hippocratic_3.0-lightgrey.svg",
+    ibm: "License-IPL_1.0-blue.svg",
+    isc: "License-ISC-blue.svg",
+    mit: "License-MIT-yellow.svg",
+    mozilla: "License-MPL_2.0-brightgreen.svg",
+    attribution: "License-ODC_BY-brightgreen.svg",
+    openDataBase: "License-ODbL-brightgreen.svg",
+    publicDomain: "License-PDDL-brightgreen.svg",
+    perl: "License-Perl-0298c3.svg",
+    artistic: "License-Artistic_2.0-0298c3.svg",
+    sil: "License-OFL_1.1-lightgreen.svg",
+    unlicense: "license-Unlicense-blue.svg",
+    wtfpl: "License-WTFPL-brightgreen.svg",
+    zlib: "License-Zlib-lightgrey.svg"
+});
+
+const creativeCommonLicenseUrlQuery = Object.freeze({
+    cco: Object.freeze({
+        shield: "License-CC0_1.0-lightgrey.svg",
+        button: "zero/1.0/80x15.png"
+    }),
+    attribution4: Object.freeze({
+        shield: "License-CC_BY_4.0-lightgrey.svg",
+        button: "by/4.0/80x15.png"
+    }),
+    attribution4ShareAlike: Object.freeze({
+        shield: "License-CC_BY--SA_4.0-lightgrey.svg",
+        button: "by-sa/4.0/80x15.png"
+    }),
+    attributionNonCommercial: Object.freeze({
+        shield: "License-CC_BY--NC_4.0-lightgrey.svg",
+        button: "by-nc/4.0/80x15.png"
+    }),
+    attributionNoDerivatives: Object.freeze({
+        shield: "License-CC_BY--ND_4.0-lightgrey.svg",
+        button: "by-nd/4.0/80x15.png"
+    }),
+    attributionNonCommercialShareAlike: Object.freeze({
+        shield: "License-CC_BY--NC--SA_4.0-lightgrey.svg",
+        button: "by-nc-sa/4.0/80x15.png"
+    }),
+    attributionNonCommercialNoDerivatives: Object.freeze({
+        shield: "License-CC_BY--NC--ND_4.0-lightgrey.svg",
+        button: "by-nc-nd/4.0/80x15.png"
+    })
+});
