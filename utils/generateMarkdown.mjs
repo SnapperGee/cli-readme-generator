fd9bc8d77169ec29a960b81af80ac5c56a0d52f6
Snap <XxSnapperGeexX@gmail.com>
2023-09-28 18:12:20 -0700

    Add table of contents section and begin adding license section.

--- utils/generateMarkdown.mjs
+++ utils/generateMarkdown.mjs
@@ -19,33 +19,48 @@ export const generateMarkdown = (data) => {
         `# ${data.title}\n\n${renderLicenseLink(licenseValues.find(licenseValue => licenseValue.name === data.license))}`
     ];
 
+    const tableOfContentsLines = [
+        "\n## Table of contents\n"
+    ];
+
     if (data.description)
     {
+        tableOfContentsLines.push(`1. [Description](#description)`);
         markdownLines.push(`\n\n## Description\n\n${data.description}`);
     }
 
     if (data.installation)
     {
+        tableOfContentsLines.push(`1. [Installation](#installation)`);
         markdownLines.push(`\n\n## Installation\n\n${data.installation}`);
     }
 
     if (data.usage)
     {
+        tableOfContentsLines.push(`1. [Usage](#usage)`);
         markdownLines.push(`\n\n## Usage\n\n${data.usage}`);
+    }
+
+    if (data.license)
+    {
+
     }
 
     if (data.contribution)
     {
+        tableOfContentsLines.push(`1. [Contributing](#contributing)`);
         markdownLines.push(`\n\n## Contributing\n\n${data.contribution}`);
     }
 
     if (data.tests)
     {
+        tableOfContentsLines.push(`1. [Tests](#tests)`);
         markdownLines.push(`\n\n## Tests\n\n${data.tests}`);
     }
 
     if (data.github || data.email)
     {
+        tableOfContentsLines.push(`1. [Questions](#questions)`);
         markdownLines.push("\n\n## Questions\n\nAny questions can be directed to");
 
         if (data.github)
@@ -57,9 +72,15 @@ export const generateMarkdown = (data) => {
         {
             markdownLines.push(` ${data.github ? "or ": ""}${data.email}`);
         }
+
     }
 
-    return markdownLines.join("");
+    if (tableOfContentsLines.length !== 1)
+    {
+        markdownLines.splice(1, 0, tableOfContentsLines.join("\n"));
+    }
+
+    return markdownLines.join("") + "\n";
 };
 
 export default generateMarkdown;
