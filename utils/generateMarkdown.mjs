6849221e0d55cf76bfa758a7825198cfa6c89525
Snap <XxSnapperGeexX@gmail.com>
2023-09-28 18:30:24 -0700

    Implement renderLicense section how todo requests.

--- utils/generateMarkdown.mjs
+++ utils/generateMarkdown.mjs
@@ -2,15 +2,15 @@ import { license as definedLicense, licenseKeys, licenseValues } from "./license
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-export const renderLicenseBadge = (license) => `![${license.name}](${license.shieldLink})`;
+export const renderLicenseBadge = (license) => license ? `![${license.name}](${license.shieldLink})` : "";
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) => `[${renderLicenseBadge(license)}](${license.clauseLink})`;
+const renderLicenseLink = (license) => license ? `[${renderLicenseBadge(license)}](${license.clauseLink})` : "";
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = (license) => licenseKeys.includes(license) ? `${renderLicenseLink(license)}\n${license.name}` : "";
+const renderLicenseSection = (license) => license ? ["1. [License](#license)", `\n\n## License\n\n### ${license.name}`] : "";
 
 // TODO: Create a function to generate markdown for README
 export const generateMarkdown = (data) => {
@@ -25,43 +25,43 @@ export const generateMarkdown = (data) => {
 
     if (data.description)
     {
-        tableOfContentsLines.push(`1. [Description](#description)`);
+        tableOfContentsLines.push("1. [Description](#description)");
         markdownLines.push(`\n\n## Description\n\n${data.description}`);
     }
 
     if (data.installation)
     {
-        tableOfContentsLines.push(`1. [Installation](#installation)`);
+        tableOfContentsLines.push("1. [Installation](#installation)");
         markdownLines.push(`\n\n## Installation\n\n${data.installation}`);
     }
 
     if (data.usage)
     {
-        tableOfContentsLines.push(`1. [Usage](#usage)`);
+        tableOfContentsLines.push("1. [Usage](#usage)");
         markdownLines.push(`\n\n## Usage\n\n${data.usage}`);
     }
 
     if (data.license)
     {
-        tableOfContentsLines.push(`1. [License](#license)`);
+        tableOfContentsLines.push("1. [License](#license)");
         markdownLines.push(`\n\n## License\n\n### ${data.license}`);
     }
 
     if (data.contribution)
     {
-        tableOfContentsLines.push(`1. [Contributing](#contributing)`);
+        tableOfContentsLines.push("1. [Contributing](#contributing)");
         markdownLines.push(`\n\n## Contributing\n\n${data.contribution}`);
     }
 
     if (data.tests)
     {
-        tableOfContentsLines.push(`1. [Tests](#tests)`);
+        tableOfContentsLines.push("1. [Tests](#tests)");
         markdownLines.push(`\n\n## Tests\n\n${data.tests}`);
     }
 
     if (data.github || data.email)
     {
-        tableOfContentsLines.push(`1. [Questions](#questions)`);
+        tableOfContentsLines.push("1. [Questions](#questions)");
         markdownLines.push("\n\n## Questions\n\nAny questions can be directed to");
 
         if (data.github)
