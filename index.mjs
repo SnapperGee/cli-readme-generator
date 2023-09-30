52ce19d1cbed34ecf89273e00eda7dc812f260f0
Snap <XxSnapperGeexX@gmail.com>
2023-09-30 00:22:59 -0700

    Make init function synchronous and execute all logic in then of prompt call.

--- index.mjs
+++ index.mjs
@@ -7,47 +7,29 @@ const questions = Object.values(question);
 
 const writeToFile = async (fileName, data, callback) => writeFile(fileName, data, callback);
 
-export const init = async () =>
+export const init = () =>
 {
-    let answers = await inquirer.prompt(questions);
-
-    while (answers.overwrite === false)
+    inquirer.prompt(questions).then( async (answers) =>
     {
-        delete answers.outputFilepath;
-        delete answers.overwrite;
-        answers = await inquirer.prompt([question.outputFilepath, question.overwrite, question.confirm], answers);
-    }
+        while (answers.overwrite === false)
+        {
+            delete answers.outputFilepath;
+            delete answers.overwrite;
+            answers = await inquirer.prompt([question.outputFilepath, question.overwrite, question.confirm], answers);
+        }
 
-    if (answers.confirm === true)
-    {
-        writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
-            if (err) { throw err; }
-            console.log(`README generated at: "${answers.outputFilepath}"`);
-        });
-    }
-    else
-    {
-        console.log(`Generation of "${answers.outputFilepath}" rejected. Aborting...`);
-    }
-
-    // inquirer.prompt(questions).then( (answers) =>
-    // {
-    //     if (answers.overwrite === false)
-    //     {
-    //         console.log(`Overwrite of "${answers.outputFilepath}" rejected. Aborting...`);
-    //     }
-    //     else if (answers.confirm === true)
-    //     {
-    //         writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
-    //             if (err) { throw err; }
-    //             console.log(`README generated at: "${answers.outputFilepath}"`);
-    //         });
-    //     }
-    //     else
-    //     {
-    //         console.log(`Generation of "${answers.outputFilepath}" rejected. Aborting...`);
-    //     }
-    // });
+        if (answers.confirm === true)
+        {
+            writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
+                if (err) { throw err; }
+                console.log(`README generated at: "${answers.outputFilepath}"`);
+            });
+        }
+        else
+        {
+            console.log(`Generation of "${answers.outputFilepath}" rejected. Aborting...`);
+        }
+    });
 };
 
 init();
