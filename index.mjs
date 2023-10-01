2e591d261da3b70cf5e3b37ef473c03ded5c17a1
Snap <XxSnapperGeexX@gmail.com>
2023-10-01 09:47:58 -0700

    Start implementing edit answers loop in index.

--- index.mjs
+++ index.mjs
@@ -7,44 +7,74 @@ const questions = Object.values(question);
 
 const writeToFile = async (fileName, data, callback) => writeFile(fileName, data, callback);
 
-export const init = () =>
+export const init = async () =>
 {
-    inquirer.prompt(questions).then( async (answers) =>
+    let answers = await inquirer.prompt(questions);
+
+    while(answers.confirm !== true)
     {
-        while (answers.overwrite === false)
+        delete answers.confirm;
+
+        while(answers.overwrite === false)
         {
-            await inquirer.prompt(overwriteQuestions).then( (overwriteAnswers) =>
-            {
-                answers.outputFilepath = overwriteAnswers.outputFilepath;
-                answers.overwrite = overwriteAnswers.overwrite;
-                answers.confirm = overwriteAnswers.confirm;
-            });
+            delete answers.outputFilepath;
+            delete answers.overwrite;
+
+            answers = await inquirer.prompt(overwriteQuestions, answers);
         }
 
-        while (answers.confirm === false)
+        await inquirer.prompt(editAnswersQuestion, answers).then( async (answersToEdit) =>
         {
-            delete answers.confirm;
-
-            await inquirer.prompt(editAnswersQuestion, answers).then( async (answersToEdit) =>
+            await inquirer.prompt(questions.filter(question => answersToEdit.editAnswers.name === question.name)).then( (newAnswers) =>
             {
-                await inquirer.prompt(questions.filter(question => answersToEdit.editAnswers.includes(question.name))).then( (newAnswers) =>
+                for (const newAnswer of Object.values(newAnswers))
                 {
-                    for (const newAnswer of Object.entries(newAnswers))
-                    {
-                        answers[newAnswer[0]] = newAnswer[1];
-                    }
-
-                });
-
-                answers = await inquirer.prompt(question.confirm, answers);
+                    answers[newAnswer.name] = newAnswer;
+                }
             });
-        }
-
-        writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
-            if (err) { throw err; }
-            console.log(`README generated at: "${answers.outputFilepath}"`);
         });
+
+        answers = await inquirer.prompt(question.confirm, answers);
+    }
+
+    writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
+        if (err) { throw err; }
+        console.log(`README generated at: "${answers.outputFilepath}"`);
     });
+
+    // inquirer.prompt(questions).then( async (answers) =>
+    // {
+    //     while (answers.overwrite === false)
+    //     {
+    //         await inquirer.prompt(overwriteQuestions).then( (overwriteAnswers) =>
+    //         {
+    //             answers.outputFilepath = overwriteAnswers.outputFilepath;
+    //             answers.overwrite = overwriteAnswers.overwrite;
+    //             answers.confirm = overwriteAnswers.confirm;
+    //         });
+
+    //         while (answers.confirm === false)
+    //         {
+    //             delete answers.confirm;
+
+    //             await inquirer.prompt(editAnswersQuestion, answers).then( async (answersToEdit) =>
+    //             {
+    //                 await inquirer.prompt(questions.filter(question => answersToEdit.editAnswers.includes(question.name))).then( (newAnswers) =>
+    //                 {
+    //                     for (const newAnswer of Object.entries(newAnswers))
+    //                     {
+    //                         answers[newAnswer[0]] = newAnswer[1];
+    //                     }
+    //                 });
+    //             });
+    //         }
+    //     }
+
+    //     writeToFile(answers.outputFilepath, generateMarkdown(answers), (err) => {
+    //         if (err) { throw err; }
+    //         console.log(`README generated at: "${answers.outputFilepath}"`);
+    //     });
+    // });
 };
 
 init();
