60784d705db6aa1914c21667295dacb74ddfe102
Snap <XxSnapperGeexX@gmail.com>
2023-09-30 17:41:52 -0700

    Import outputFilepath, overwrite, and confirm questions array and use in awaited fetch then of new prompt if overwrtie is false.

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { question } from "./utils/question.mjs";
+import { question, overwriteQuestions } from "./utils/question.mjs";
 import { generateMarkdown } from "./utils/generateMarkdown.mjs";
 import inquirer from "inquirer";
 import { writeFile } from "node:fs";
@@ -7,17 +7,18 @@ const questions = Object.values(question);
 
 const writeToFile = async (fileName, data, callback) => writeFile(fileName, data, callback);
 
-const outputFilepathOverwriteConfirmQuestions = Object.freeze([question.outputFilepath, question.overwrite, question.confirm]);
-
 export const init = () =>
 {
     inquirer.prompt(questions).then( async (answers) =>
     {
         while (answers.overwrite === false)
         {
-            delete answers.outputFilepath;
-            delete answers.overwrite;
-            answers = await inquirer.prompt(outputFilepathOverwriteConfirmQuestions, answers);
+            await inquirer.prompt(overwriteQuestions).then(overwriteAnswers =>
+            {
+                answers.outputFilepath = overwriteAnswers.outputFilepath;
+                answers.overwrite = overwriteAnswers.overwrite;
+                answers.confirm = overwriteAnswers.confirm;
+            });
         }
 
         if (answers.confirm === true)
