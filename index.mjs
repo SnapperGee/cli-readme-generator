2bb75de28bf0228357ffdee82173bd465dd2db21
Snap <XxSnapperGeexX@gmail.com>
2023-10-01 21:11:09 -0700

    Make overwrite answers confirmed on initial ask and fixup answer confirmation loop.

--- index.mjs
+++ index.mjs
@@ -9,29 +9,43 @@ const writeToFile = async (fileName, data, callback) => writeFile(fileName, data
 
 export const init = async () =>
 {
-    let answers = await inquirer.prompt(questions);
+    let answers = await inquirer.prompt(questions).then( async (initAnswers) =>
+    {
+        while(initAnswers.overwrite === false)
+        {
+            delete initAnswers.outputFilepath;
+            delete initAnswers.overwrite;
+
+            initAnswers = await inquirer.prompt([question.outputFilepath, question.overwrite], initAnswers);
+        }
+
+        return initAnswers;
+    });
 
     while(answers.confirm === false)
     {
         delete answers.confirm;
 
-        while(answers.overwrite === false)
+        const answersToEdit = await inquirer.prompt(editAnswersQuestion, answers).then(anwersWithAnswersToEdit => anwersWithAnswersToEdit.answersToEdit);
+
+        console.log(`\n\n\n${Object.entries(answersToEdit)}\n\n\n`);
+
+        if (answersToEdit.includes(question.outputFilepath) || answersToEdit.includes(question.overwrite))
         {
             delete answers.outputFilepath;
             delete answers.overwrite;
 
-            answers = await inquirer.prompt(overwriteQuestions, answers);
+            answers = await inquirer.prompt([question.outputFilepath, question.overwrite], answers);
         }
 
-        const answersToEdit = await inquirer.prompt(editAnswersQuestion, answers);//.then( async (answersToEdit) =>
-
-        const newAnswers = await inquirer.prompt(answersToEdit.editAnswers.map(answerToEdit => questions.find(question => answerToEdit === question.name))).catch(err => {throw new err});
-
-        console.log(newAnswers);
-
-        for (const newAnswer in newAnswers)
+        if (answersToEdit.length !== 0)
         {
-            answers[newAnswer] = newAnswers[newAnswer];
+            const newAnswers = await inquirer.prompt(answersToEdit.map(answerToEdit => questions.find(question => answerToEdit === question.name))).catch(err => {throw new err});
+
+            for (const newAnswer in newAnswers)
+            {
+                answers[newAnswer] = newAnswers[newAnswer];
+            }
         }
 
         answers = await inquirer.prompt(question.confirm, answers);
